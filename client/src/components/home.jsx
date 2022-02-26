b50faa4800d4c609bca8d159b0fa6b44b45d9e65
dipesh809 <b20096@students.iitmandi.ac.in>
2022-02-26 23:53:31 +0530

    added home content

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import { Button } from "react-bootstrap"
 
 function Home() {
   return (
     <div>
-        <div className="home">
-          
+      <div className='home'>
+      <div className="container">
+          <h1>Lorem ipsum dolor sit.</h1>
         </div>
+        <div className="content">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores nisi, hic optio tempore nobis rem.
+        </div>
+        <div className="buttons">
+          <Button primary className='mx-3'>Get started</Button>
+          <Button primary className='mx-3'>Get started</Button>
+        </div>
+      </div>
+        
     </div>
   )
 }
